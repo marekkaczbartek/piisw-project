5c3bdfeb7582b134c990bf65c47d196d4129f199
marekkaczbartek <bartek.kaczmarek03@gmail.com>
2026-04-23 17:33:33 +0200

    init

---
+++ eticket-project-backend/src/main/java/org/example/eticket/EticketProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.eticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EticketProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EticketProjectBackendApplication.class, args);
+    }
+
+}
---
+++ eticket-project-backend/src/test/java/org/example/eticket/EticketProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.eticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EticketProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
