c1c3d8310da3cd57be102ce80b58ce3efc055de3
marekkaczbartek <bartek.kaczmarek03@gmail.com>
2026-05-19 14:19:57 +0200

    chore: make endpoint patch

--- eticket-project-backend/src/main/java/org/example/eticket/api/controller/PurchaseController.java
+++ eticket-project-backend/src/main/java/org/example/eticket/api/controller/PurchaseController.java
@@ -23,6 +23,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -124,7 +125,7 @@ public class PurchaseController {
         return ResponseEntity.ok(response);
     }
 
-    @PostMapping("/{purchaseId}/punch")
+    @PatchMapping("/{purchaseId}/punch")
     public ResponseEntity<PunchTicketResponse> punchTicket(
             @PathVariable UUID purchaseId,
             @RequestBody PunchTicketRequest request
